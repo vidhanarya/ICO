1c7c0645c67897fa50bf33bd75aa1ee410652b7b
vidhanarya <vidhanarya50@gmail.com>
2022-10-30 15:30:17 +0530

    CryptoDevs ICO

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  "0xB307218AeDCE45c697Af1DCa5B0D6C7d4a63e237";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
